ab74b9b5ee7948c41ae1e542a2eb0a7b6a47d286
YugaByte CI <yugabyte-ci@users.noreply.github.com>
2022-06-30 00:01:20 +0000

    Update the version to 2.15.0.0

--- pkg/controller/ybcluster/ybcluster_controller.go
+++ pkg/controller/ybcluster/ybcluster_controller.go
@@ -65,9 +65,9 @@ const (
 	envPodIPVal                 = "status.podIP"
 	envPodName                  = "POD_NAME"
 	envPodNameVal               = "metadata.name"
-	yugabyteDBImageName         = "yugabytedb/yugabyte:2.13.2.0-b135"
+	yugabyteDBImageName         = "yugabytedb/yugabyte:2.15.0.0-b11"
 	imageRepositoryDefault      = "yugabytedb/yugabyte"
-	imageTagDefault             = "2.13.2.0-b135"
+	imageTagDefault             = "2.15.0.0-b11"
 	imagePullPolicyDefault      = corev1.PullIfNotPresent
 	podManagementPolicyDefault  = appsv1.ParallelPodManagement
 	storageCountDefault         = int32(1)
